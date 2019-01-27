cb1739f9524971a47ee0f78264a15f025010f716
Daniel Barkley <danielbarkley@daniels-mbp.attlocal.net>
2019-01-26 21:51:38 -0600

    Fixed button bug, changed background, added API call to Giphy.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,24 @@
-var buttons = ["Donkey Kong", "Tetris", "Pacman", "Galaga", "Frogger", "Defender", "Mario Bros", "Missile Command", "Star Wars", "Duck Hunt"]
+var buttons = ["Tetris", "Pacman", "Galaga", "Frogger", "Defender", "Mario Bros", "Star Wars", "Duck Hunt", "Missile Command", "Donkey Kong"]
+
+function getGiphy() {
+    var giphy = $(this).attr("data-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + giphy + "&api_key=b7EI7s7YtuxvcD1MnXmr6c5nh7gYF9Pt&limit=10&rating=g"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    })
+}
+
 
 function makeButtons() {
     $("#button-holder").empty();
 
     for (var i = 0; i < buttons.length; i++) {
         var b = $("<button>");
-        b.addClass("btn, btn-primary, mb-2");
+        b.addClass("btn btn-primary mb-2 retro");
         b.attr("data-name", buttons[i]);
         b.text(buttons[i]);
         $("#button-holder").append(b);
@@ -20,5 +33,7 @@ $(".btn").on("click", function(e) {
 
     makeButtons();
 });
+
+$(document.body).on("click", ".retro", getGiphy);
 
 makeButtons();
