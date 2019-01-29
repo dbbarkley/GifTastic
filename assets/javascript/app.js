3d2e15afc26f13278755fffeb1bec3f8dd86049f
Daniel Barkley <danielbarkley@Daniels-MacBook-Pro.local>
2019-01-29 08:56:58 -0600

    fixed bugs on 'click' events.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,13 +4,26 @@ function getGiphy() {
     var giphy = $(this).attr("data-name");
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + giphy + "&api_key=b7EI7s7YtuxvcD1MnXmr6c5nh7gYF9Pt&limit=10&rating=g"
 
+    $(".giphy-holder").empty();
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
         console.log(response);
-    })
-}
+        for(var i = 0; i <= 9; i++) {
+
+            var rating = response.data[i].rating;
+            var url = response.data[i].images.original.url;
+            var output = "<div class='giphy-div'><h4 id='ratings'>Rating: " + rating + "</h4>" +
+                        "<img id='new-giphy' width='300px' src='" + url + "'/></div>";
+            console.log(rating);
+            console.log(url);
+            console.log(output);
+            $(".giphy-holder").append(output);
+
+        };
+    });
+};
 
 
 function makeButtons() {
