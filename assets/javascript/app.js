750602720d7140e0ca2adbebefc8ddbaeb854225
Daniel Barkley <danielbarkley@daniels-mbp-2.attlocal.net>
2019-01-30 07:22:18 -0600

    Added comments in JS and fixed a few bugs.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,34 @@
+// My array of stings/giphy's
 var buttons = ["Tetris", "Pacman", "Galaga", "Frogger", "Defender", "Mario Bros", "Star Wars", "Duck Hunt", "Missile Command", "Donkey Kong"]
 
 function getGiphy() {
-    var giphy = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + giphy + "&api_key=b7EI7s7YtuxvcD1MnXmr6c5nh7gYF9Pt&limit=10&rating=g"
 
-    $(".giphy-holder").empty();
+    // Grab data-names from my buttons array.
+    var giphy = $(this).attr("data-name");
+
+    // Var tha holds our API URL
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + giphy + "&api_key=b7EI7s7YtuxvcD1MnXmr6c5nh7gYF9Pt&limit=10&rating=pg"
+    
+    // Empty the giphs before running another API call.
+    $(".giphy-holder").empty().removeClass("hidden");
+    
+    // Calling the Giphy API
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
         console.log(response);
-        for(var i = 0; i <= 9; i++) {
 
+        // Looping through the response data array.
+        for(var i = 0; i < 10; i++) {
+            // Var's of response data
             var rating = response.data[i].rating;
             var url = response.data[i].images.original.url;
+            // Var that holds dynamic HTML
             var output = "<div class='giphy-div'><h4 id='ratings'>Rating: " + rating + "</h4>" +
-                        "<img id='new-giphy' width='300px' src='" + url + "'/></div>";
-            console.log(rating);
-            console.log(url);
-            console.log(output);
+                        "<img id='new-giphy' width='300px' height='250px' src='" + url + "'/></div>";
+
+            // Add giphy's to page
             $(".giphy-holder").append(output);
 
         };
@@ -27,26 +37,33 @@ function getGiphy() {
 
 
 function makeButtons() {
+    // Empty all buttons before adding them again.
     $("#button-holder").empty();
 
-    for (var i = 0; i < buttons.length; i++) {
-        var b = $("<button>");
-        b.addClass("btn btn-primary mb-2 retro");
-        b.attr("data-name", buttons[i]);
-        b.text(buttons[i]);
-        $("#button-holder").append(b);
+        for (var i = 0; i < buttons.length; i++) {
+             var b = $("<button>");
+             b.addClass("btn btn-primary mb-2 retro");
+             b.attr("data-name", buttons[i]);
+             b.text(buttons[i]);
+             $("#button-holder").append(b);
     }
 };
 $(".btn").on("click", function(e) {
     e.preventDefault();
-
+    // Get value from input and check if it is blank.
     var button = $(".input").val().trim();
-
+        //if(!$(this.value).length) {
+          //  alert("Input cannot be blank");
+       // };
+    // Add new button into my buttons array.
     buttons.push(button);
+
+    // Delete input field after submit.
+    $(".input").val("");
 
     makeButtons();
 });
-
+// Click event for buttons added dynamically.
 $(document.body).on("click", ".retro", getGiphy);
 
 makeButtons();
